27995655f6eb9012cdd85ed35e2054e2eda7adc5
Dipak Singh <dipaksingh3323@gmail.com>
2025-04-05 13:11:28 +0530

    course-Details page Done and loading done

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
+import humanizeDuration from 'humanize-duration';
 
 export const AppContext = createContext();
 
@@ -26,7 +27,36 @@ export const AppContextProvider = ({ children }) => {
             totalRating += rating.rating
         })
         return totalRating / course.courseRatings.length
+    };
+
+    // Function to calculate Course Chapter Time
+    const calculateChapterTime = (chapter) => {
+        let time = 0;
+        chapter.chapterContent.map((lecture) => time += lecture.lectureDuration);
+        return humanizeDuration(time * 60 * 1000, { units: ["h", "m"] });
+    };
+
+    // Function to Calculate Course Duration
+    const calculateCourseDuration = (course) => {
+        let time = 0;
+
+        course.courseContent.map((chapter) => chapter.chapterContent.map(
+            (lecture) => time += lecture.lectureDuration
+        ));
+        return humanizeDuration(time * 60 * 1000, { units: ["h", "m"] });
+    };
+
+    // Function Calculate to No of Lectures in the course.
+    const calculateNoOfLectures = (course) => {
+        let totalLectures = 0;
+        course.courseContent.forEach(chapter => {
+            if (Array.isArray(chapter.chapterContent)) {
+                totalLectures += chapter.chapterContent.length
+            }
+        });
+        return totalLectures;
     }
+
 
     useEffect(() => {
         fetchAllCourses();
@@ -35,7 +65,7 @@ export const AppContextProvider = ({ children }) => {
 
 
     const value = {
-        currency, allCourses, navigate, calculateRating, isEducator, setIsEducator
+        currency, allCourses, navigate, calculateRating, isEducator, setIsEducator, calculateChapterTime, calculateCourseDuration, calculateNoOfLectures
     }
     return (
         <AppContext.Provider value={value}>
